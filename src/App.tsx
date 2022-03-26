5ed40d2051568540c00085939b9a33b6ae4abbf1
David Orson <onlyorson@outlook.com>
2022-03-26 23:34:48 +0100

    routing

--- src/App.tsx
+++ src/App.tsx
@@ -2,20 +2,29 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
+// mui
+import Container from "@mui/material/Container";
+
 // views
 import Home from "./ui/views/home";
+import Article from "./ui/views/article";
 
 // components
-import Navbar from './ui/components/navbar';
+import Navbar from "./ui/components/navbar";
 
 function App() {
-	return (
-		<div className="App">
+    return (
+        <div className="App h-screen pt-24">
             <Router>
-                <Navbar/>
-				<Home />
-			</Router>
-		</div>
+                <Navbar />
+                <Container>
+                    <Routes>
+                        <Route path="/" element={<Home />} />
+                        <Route path="/article" element={<Article />} />
+                    </Routes>
+                </Container>
+            </Router>
+        </div>
     );
 }
 
